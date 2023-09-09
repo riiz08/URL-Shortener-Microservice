4db3475ee2b3e7457c6a37e88a9987147daf416a
riiz08 <mhmdrizki0812@gmail.com>
2023-09-09 21:59:55 +0000

    final step

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
+
+const generateShortUrl = () => {
+  return Math.floor(Math.random() * 1000) + 1;
+};
+
+// Daftar penyimpanan untuk URL pendek
+const urlStorage = new Map();
+let shortUrlCounter = 1;
+
 
 app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
 
-  let urlDatabase = [];
-let nextShortUrl = 1;
+    // Validasi URL harus dimulai dengan "https://"
+  const urlRegex = /^https:\/\/.*/;
+
+  if (!urlRegex.test(originalUrl)) {
+    return res.json({ error: 'Invalid URL' });
+  }
 
   
-
-  // Generate a short URL
-  const shortUrl = nextShortUrl++;
-  urlDatabase.push({ originalUrl, shortUrl });
+const shortUrl = shortUrlCounter++;
+  urlStorage.set(shortUrl, originalUrl);
 
   res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-  const shortUrl = parseInt(req.params.shortUrl);
+// Rute untuk mengarahkan ke URL asli
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
 
-  // Find the original URL in the database
-  const entry = urlDatabase.find((entry) => entry.shortUrl === shortUrl);
-
-  if (!entry) {
-    return res.json({ error: 'short url not found' });
+  if (urlStorage.has(shortUrl)) {
+    const originalUrl = urlStorage.get(shortUrl);
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'URL pendek tidak ditemukan' });
   }
-
-  res.redirect(entry.originalUrl);
 });
 
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
 
+  if (urlStorage.has(shortUrl)) {
+    const originalUrl = urlStorage.get(shortUrl);
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'URL pendek tidak ditemukan' });
+  }
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
